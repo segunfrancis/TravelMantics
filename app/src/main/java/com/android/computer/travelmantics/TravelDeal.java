6af173efbc43c3583926e8f991472ec6986ba814
Segun Francis <francis4dblues@gmail.com>
2019-08-01 07:18:37 +0100

    implementation: parcelable

--- app/src/main/java/com/android/computer/travelmantics/TravelDeal.java
+++ app/src/main/java/com/android/computer/travelmantics/TravelDeal.java
@@ -1,4 +1,109 @@
 package com.android.computer.travelmantics;
 
-public class TravelDeal {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class TravelDeal implements Parcelable {
+    private String id;
+    private String title;
+    private String description;
+    private String price;
+    private String imageUrl;
+    private String imageName;
+
+    public TravelDeal() {
+    }
+
+    public TravelDeal(String id, String title, String description, String price, String imageUrl, String imageName) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.imageName = imageName;
+    }
+
+    protected TravelDeal(Parcel in) {
+        id = in.readString();
+        title = in.readString();
+        description = in.readString();
+        price = in.readString();
+        imageUrl = in.readString();
+        imageName = in.readString();
+    }
+
+    public static final Creator<TravelDeal> CREATOR = new Creator<TravelDeal>() {
+        @Override
+        public TravelDeal createFromParcel(Parcel in) {
+            return new TravelDeal(in);
+        }
+
+        @Override
+        public TravelDeal[] newArray(int size) {
+            return new TravelDeal[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(title);
+        dest.writeString(description);
+        dest.writeString(price);
+        dest.writeString(imageUrl);
+        dest.writeString(imageName);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
 }
